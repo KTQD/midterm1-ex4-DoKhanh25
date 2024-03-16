14c1fdd1698c745b911980eae873ec6d7e971029
ASUS_ROG\MyPC <tk25042002@gmail.com>
2024-03-16 14:22:50 +0700

    bai tap 4

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
-    
+        new Thread(() -> {
+            Integer highestNum = numbers[0];
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] > highestNum) {
+                    highestNum = numbers[i];
+                }
+            }
+            System.out.println("Phần tử lớn nhất trong mảng là: " + highestNum);
+        }).start();
+        new Thread(() -> {
+            Integer sum = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                sum += numbers[i];
+            }
+            System.out.println("Tổng của các phần tử trong mảng là: " + sum);
+        }).start();
 
 
-        Viết mã của bạn vào đây
-    
+
+
     }
 }
